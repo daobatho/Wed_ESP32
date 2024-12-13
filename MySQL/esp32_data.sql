287cfac95400c92fc4265a9b995c455d48268af2
DESKTOP-JUBBHT8\ThoDB <thodaoba@gmail.com>
2024-12-13 10:16:56 +0700

    .

--- MySQL/esp32_data.sql
+++ MySQL/esp32_data.sql
@@ -1,3 +1,11 @@
 USE esp32_data;
+CREATE TABLE sensor_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Temp FLOAT,
+    Humid FLOAT,
+    Light BIGINT,
+    Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Wind INT
+);
 SELECT * FROM sensor_data
 
